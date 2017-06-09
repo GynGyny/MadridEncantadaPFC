8b2a135077bcdbddd19678dc25822397efb613a2
GynGyny <vgarcia5583@gmail.com>
2017-06-09 19:41:10 +0200

    acceso a la geolocalización por Google Play Services

--- app/src/main/java/geo/rutas/madrid/com/madridencantada/activities/AudioMapsActivity.java
+++ app/src/main/java/geo/rutas/madrid/com/madridencantada/activities/AudioMapsActivity.java
@@ -1,17 +1,31 @@
 package geo.rutas.madrid.com.madridencantada.activities;
 
 import android.Manifest;
+import android.content.DialogInterface;
 import android.content.pm.PackageManager;
+import android.location.Location;
 import android.media.MediaPlayer;
+import android.os.Build;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
 import android.support.v4.view.PagerTitleStrip;
+import android.support.v7.app.AlertDialog;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.TextView;
 import android.widget.Toast;
 
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.GoogleApiAvailability;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.location.LocationListener;
+import com.google.android.gms.location.LocationRequest;
+import com.google.android.gms.location.LocationServices;
 import com.google.android.gms.maps.CameraUpdate;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
@@ -29,16 +43,26 @@ import application.MadridEncantadaApp;
 import geo.rutas.madrid.com.madridencantada.R;
 import models.Lugar;
 
-public class AudioMapsActivity extends FragmentActivity implements OnMapReadyCallback, View.OnClickListener {
+public class AudioMapsActivity extends AppCompatActivity implements OnMapReadyCallback, View.OnClickListener, GoogleApiClient.ConnectionCallbacks,
+        GoogleApiClient.OnConnectionFailedListener, LocationListener {
 
     private GoogleMap mMap;
     private ImageView ivRew;
     private ImageView ivPlay;
     private ImageView ivPause;
+    private TextView tvMiniMapName;
     List<Marker> markers = new ArrayList<Marker>(); //Lista para los puntos del mapa
     private MediaPlayer mediaPlayer;
-    private int backwardTime = 5000;
-    private double startTime = 0;
+
+    Location mLocation;
+    GoogleApiClient mGoogleApiClient;
+
+    private LocationRequest mLocationRequest;
+    private long UPDATE_INTERVAL = 15000;  /* 15 secs */
+    private long FASTEST_INTERVAL = 5000; /* 5 secs */
+
+    private static double ACTION_RADIUS = 15.0;
+
 
 
     @Override
@@ -52,8 +76,16 @@ public class AudioMapsActivity extends FragmentActivity implements OnMapReadyCal
         ivPause = (ImageView) findViewById(R.id.iv_media_pause);
         ivPlay = (ImageView) findViewById(R.id.iv_media_play);
         ivRew = (ImageView) findViewById(R.id.iv_media_rew);
-        addOnclickListener();
+        tvMiniMapName = (TextView) findViewById(R.id.tv_MiniMapName);
+        addOnclickListeners();
+
+        mGoogleApiClient = new GoogleApiClient.Builder(this)
+                .addApi(LocationServices.API)
+                .addConnectionCallbacks(this)
+                .addOnConnectionFailedListener(this)
+                .build();
     }
+
 
 
     @Override
@@ -80,6 +112,8 @@ public class AudioMapsActivity extends FragmentActivity implements OnMapReadyCal
         mMap.setMyLocationEnabled(true);
     }
 
+
+
     private void animateGoogleMapCamera() {
         //Android map v2 zoom para mostrar los marcadores
         LatLngBounds.Builder builder = new LatLngBounds.Builder();
@@ -92,11 +126,15 @@ public class AudioMapsActivity extends FragmentActivity implements OnMapReadyCal
         mMap.moveCamera(cu);
     }
 
-    private void addOnclickListener(){
+
+
+    private void addOnclickListeners(){
         ivRew.setOnClickListener(this);
         ivPlay.setOnClickListener(this);
         ivPause.setOnClickListener(this);
     }
+
+
 
     @Override
     public void onClick(View v) {
@@ -108,6 +146,8 @@ public class AudioMapsActivity extends FragmentActivity implements OnMapReadyCal
             rewAudio();
     }
 
+
+
     public void playAudio (){
         if (mediaPlayer==null){
             mediaPlayer = MediaPlayer.create(this, R.raw.bach);
@@ -115,37 +155,22 @@ public class AudioMapsActivity extends FragmentActivity implements OnMapReadyCal
         } else {
             mediaPlayer.start();
         }
-        startTime = mediaPlayer.getCurrentPosition();
-
     }
+
+
 
     public void pauseAudio (){
         if (mediaPlayer.isPlaying())
             mediaPlayer.pause();
     }
 
+
+
     public void rewAudio (){
         mediaPlayer.seekTo(0);
         mediaPlayer.pause();
     }
-        /**
-         * // When user click to "Rewind".
-         public void doRewind(View view)  {
-         int currentPosition = this.mediaPlayer.getCurrentPosition();
-         int duration = this.mediaPlayer.getDuration();
-         // 5 seconds.
-         int SUBTRACT_TIME = 5000;
 
-         if(currentPosition - SUBTRACT_TIME > 0 )  {
-         this.mediaPlayer.seekTo(currentPosition - SUBTRACT_TIME);
-         }
-         }
-         */
-        /**if (mediaPlayer.isPlaying()) {
-            mediaPlayer.stop();
-            mediaPlayer.reset();
-            playAudio();
-        }*/
 
 
     @Override
@@ -156,4 +181,89 @@ public class AudioMapsActivity extends FragmentActivity implements OnMapReadyCal
             mediaPlayer.reset();
         }
     }
+
+
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        if (mGoogleApiClient != null) {
+            mGoogleApiClient.connect();
+        }
+    }
+
+
+
+    @Override
+    public void onConnectionSuspended(int i) {
+    }
+
+
+
+    @Override
+    public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
+    }
+
+
+
+    @Override
+    public void onConnected(@Nullable Bundle bundle) {
+        if (ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            // TODO: Consider calling
+            //    ActivityCompat#requestPermissions
+            // here to request the missing permissions, and then overriding
+            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+            //                                          int[] grantResults)
+            // to handle the case where the user grants the permission. See the documentation
+            // for ActivityCompat#requestPermissions for more details.
+            return;
+        }
+        mLocation = LocationServices.FusedLocationApi.getLastLocation(mGoogleApiClient);
+        if(mLocation!=null)
+        {
+            tvMiniMapName.setText("Latitude : "+mLocation.getLatitude()+" , Longitude : "+mLocation.getLongitude());
+        }
+        startLocationUpdates();
+    }
+
+
+
+    protected void startLocationUpdates() {
+        mLocationRequest = new LocationRequest();
+        mLocationRequest.setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY);
+        mLocationRequest.setInterval(UPDATE_INTERVAL);
+        mLocationRequest.setFastestInterval(FASTEST_INTERVAL);
+        if (ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            Toast.makeText(getApplicationContext(), "Enable Permissions", Toast.LENGTH_LONG).show();
+        }
+        LocationServices.FusedLocationApi.requestLocationUpdates(
+                mGoogleApiClient, mLocationRequest, this);
+    }
+
+
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        stopLocationUpdates();
+    }
+
+
+
+    public void stopLocationUpdates() {
+        if (mGoogleApiClient.isConnected()) {
+            LocationServices.FusedLocationApi
+                    .removeLocationUpdates(mGoogleApiClient, this);
+            mGoogleApiClient.disconnect();
+        }
+    }
+
+
+    @Override
+    public void onLocationChanged(Location location) {   // CALLBACK PARA LOS CAMBIOS DE UBICACIÓN
+        if(location!=null)
+            tvMiniMapName.setText("Latitude : "+location.getLatitude()+" , Longitude : "+location.getLongitude());
+    }
+
 }
+
